77bba14bd2b6996eab67fb92249b283298ec6dc4
Matthew Steele <matthewsteeleonline@gmail.com>
2020-07-27 02:40:28 +0000

    Automatically backed up by Learn

--- lib/04_fourth_challenge.rb
+++ lib/04_fourth_challenge.rb
@@ -5,12 +5,12 @@ def fourth_challenge
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
-        Benvolio = {
+        benvolio = {
           :name => "Benvolio", 
           :age => "17",
           :attitude => "worried"
         },
-        Mercutio = {
+        mercutio = {
           :name => "Mercutio",
           :age => "18",
           :attitude => "hot-headed"
@@ -22,12 +22,12 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        Steven = {
+        steven = {
           :name => "Steven", 
           :age => "30",
           :attitude => "confused"
         },
-        Nurse = {
+        nurse = {
           :name => "Nurse",
           :age => "44",
           :attitude => "worried"
